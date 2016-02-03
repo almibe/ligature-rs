93377f5d6929e0fe2495e7ef18c86fe9b703a048
Alex Berry <berry3@indiana.edu>
2016-02-02 20:16:12 -0500

    adding rest of model and fixing package typo

--- src/main/groovy/org/libraryweasel/stinkpot/ntriples/LangLiteral.groovy
+++ src/main/groovy/org/libraryweasel/stinkpot/ntriples/LangLiteral.groovy
@@ -2,13 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.libraryweasel.stinkpot.ntrilples
+package org.libraryweasel.stinkpot.ntriples
 
 import groovy.transform.Immutable
 
 @Immutable
-class Triple {
-    Subject subject
-    Predicate predicate
-    Object object
+class LangLiteral implements Literal {
+    String langTag
 }
--- src/main/groovy/org/libraryweasel/stinkpot/ntriples/PlainLiteral.groovy
+++ src/main/groovy/org/libraryweasel/stinkpot/ntriples/PlainLiteral.groovy
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.libraryweasel.stinkpot.ntrilples
+package org.libraryweasel.stinkpot.ntriples;
 
-interface Object {}
+import groovy.transform.Immutable;
+
+@Immutable
+class PlainLiteral implements Literal {
+}
