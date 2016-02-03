629c3229c96e68389d8f97202aff1c020063939e
Alex Berry <berry3@indiana.edu>
2016-02-02 22:02:21 -0500

    switching to using custom examples based off of examples from the Specification & adding second failing test for handling multiple triples

--- src/test/groovy/org/libraryweasel/stinkpot/StinkpotSpec.groovy
+++ src/test/groovy/org/libraryweasel/stinkpot/StinkpotSpec.groovy
@@ -11,14 +11,28 @@ import spock.lang.Specification
 public class StinkpotSpec extends Specification {
     Stinkpot stinkpot = new Stinkpot()
 
-    def 'support basic triple, example 2'() {
+    def 'support basic IRI triple'() {
         given:
         def expectedResult = new Triple(new IRI("http://example.org/#spiderman"),
             new IRI("http://www.perceive.net/schemas/relationship/enemyOf"), new IRI("http://example.org/#green-goblin"))
         when:
-        List<Triple> results = stinkpot.parseTriples(this.getClass().getResource('/ntriples/specificationExamples/example2.nt').text)
+        List<Triple> results = stinkpot.parseTriples(this.getClass().getResource('/ntriples/basicTriple.nt').text)
         then:
         results.size() == 1
         results.first() == expectedResult
+    }
+
+    def 'support multiple IRI triples'() {
+        given:
+        def expectedResult1 = new Triple(new IRI("http://example.org/#spiderman"),
+                    new IRI("http://www.perceive.net/schemas/relationship/enemyOf"), new IRI("http://example.org/#green-goblin"))
+        def expectedResult2 = new Triple(new IRI("http://example.org/#spiderman"),
+                    new IRI("http://www.perceive.net/schemas/relationship/enemyOf"), new IRI("http://example.org/#black-cat"))
+        when:
+        List<Triple> results = stinkpot.parseTriples(this.getClass().getResource('/ntriples/multipleIRITriples.nt').text)
+        then:
+        results.size() == 2
+        results.first() == expectedResult1
+        results.last() == expectedResult2
     }
 }
