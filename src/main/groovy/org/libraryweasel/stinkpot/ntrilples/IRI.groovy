8686883bec6bfc0762c569b2d2c873747562e49a
Alex Berry <berry3@indiana.edu>
2016-02-02 20:01:02 -0500

    removing old code and starting to add n-triples model

--- src/main/groovy/org/libraryweasel/stinkpot/ntrilples/IRI.groovy
+++ src/main/groovy/org/libraryweasel/stinkpot/ntrilples/IRI.groovy
@@ -2,13 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.libraryweasel.stinkpot
+package org.libraryweasel.stinkpot.ntrilples
 
 import groovy.transform.Immutable
 
 @Immutable
-public class Triple {
-    String subject
-    String predicate
-    String object
+class IRI implements Subject, Predicate, Object {
+    String value;
 }
--- src/main/groovy/org/libraryweasel/stinkpot/ntrilples/Triple.groovy
+++ src/main/groovy/org/libraryweasel/stinkpot/ntrilples/Triple.groovy
@@ -2,8 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.libraryweasel.stinkpot
+package org.libraryweasel.stinkpot.ntrilples
 
-public class Stinkpot {
+import groovy.transform.Immutable
 
+@Immutable
+class Triple {
+    Subject subject
+    Predicate predicate
+    Object object
 }
