31a594c94974b422965e1701a4a65358fa54f4b2
Alex Berry <berry3@indiana.edu>
2016-02-04 22:22:23 -0500

    adding data model written in kotlin

---
+++ src/main/kotlin/org/libraryweasel/stinkpot/Model.kt
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.libraryweasel.stinkpot
+
+interface Subject {}
+interface Object {}
+interface Predicate {}
+
+data class IRI(val value: String) : Subject, Predicate, Object
+data class BlankNode(val label: String) : Subject, Object
+data class Triple(val subject: Subject, val predicate: Predicate, val `object`: Object)
+
+interface Literal { val value: String}
+data class PlainLiteral (override val value: String) : Literal
+data class LangLiteral(override val value: String, val langTag: String) : Literal
+data class TypedLiteral(override val value: String, val datatypeIRI: IRI) : Literal
