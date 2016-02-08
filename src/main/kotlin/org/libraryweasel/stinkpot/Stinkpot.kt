0121cc98b342e293b4976e680eab2e8b581ce6f6
Alex Berry <berry3@indiana.edu>
2016-02-08 01:40:26 -0500

    filling in methods

--- src/main/kotlin/org/libraryweasel/stinkpot/Stinkpot.kt
+++ src/main/kotlin/org/libraryweasel/stinkpot/Stinkpot.kt
@@ -7,18 +7,45 @@ package org.libraryweasel.stinkpot
 import org.libraryweasel.stinkpot.ntriples.NTriplesLexer
 import org.libraryweasel.stinkpot.ntriples.NTriplesParser
 import org.libraryweasel.stinkpot.ntriples.Triple
+import java.io.InputStreamReader
+import java.nio.file.Files
+import java.nio.file.Path
 import java.util.*
 
 class Stinkpot {
-    fun parseTriples(text: String) : ArrayList<Triple>  {
+    fun parseTriples(text: String) : List<Triple>  {
         val triples : ArrayList<Triple> = ArrayList()
         parseTriples(text) { triples.add(it) }
         return triples
     }
 
-    fun parseTriples(text: String, handler: (Triple) -> Unit) { //TODO this should accept a stream not a String
-        val lexer = NTriplesLexer(text)
+    fun parseTriples(text: String, handler: (Triple) -> Unit) {
+        val lexer = NTriplesLexer(StringUtil.createStringStream(text))
         val parser = NTriplesParser(lexer, handler)
         parser.start()
+    }
+
+    fun parseTriples(path: Path) : List<Triple> {
+        val triples : ArrayList<Triple> = ArrayList()
+        parseTriples(path) { triples.add(it) }
+        return triples
+    }
+
+    fun parseTriples(path: Path, handler: (Triple) -> Unit) {
+        val lexer = NTriplesLexer(Files.lines(path))
+        val parser = NTriplesParser(lexer, handler)
+        parser.start()
+    }
+
+    fun parseTurtle(text: String) : ArrayList<Triple>  {
+        throw RuntimeException()
+    }
+
+    fun parseTurtle(text: String, handler: (Triple) -> Unit) {
+        throw RuntimeException()
+    }
+
+    fun parseTurtle(reader: InputStreamReader, handler: (Triple) -> Unit) {
+        throw RuntimeException()
     }
 }
