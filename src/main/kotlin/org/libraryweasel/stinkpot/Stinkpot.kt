749bbb5801bda4ade7e8b61f0828a4c8d227a0ff
Alex Berry <berry3@indiana.edu>
2016-02-10 03:17:18 -0500

    replacing java static util class with kotlin method

--- src/main/kotlin/org/libraryweasel/stinkpot/Stinkpot.kt
+++ src/main/kotlin/org/libraryweasel/stinkpot/Stinkpot.kt
@@ -7,9 +7,12 @@ package org.libraryweasel.stinkpot
 import org.libraryweasel.stinkpot.ntriples.NTriplesLexer
 import org.libraryweasel.stinkpot.ntriples.NTriplesParser
 import org.libraryweasel.stinkpot.ntriples.Triple
+import java.io.BufferedReader
+import java.io.StringReader
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.*
+import java.util.stream.Stream
 
 class Stinkpot {
     fun parseNTriples(text: String) : List<Triple>  {
@@ -19,7 +22,7 @@ class Stinkpot {
     }
 
     fun parseNTriples(text: String, handler: (Triple) -> Unit) {
-        val lexer = NTriplesLexer(StringUtil.createStringStream(text))
+        val lexer = NTriplesLexer(createStream(text))
         val parser = NTriplesParser(lexer, handler)
         parser.start()
     }
@@ -34,5 +37,10 @@ class Stinkpot {
         val lexer = NTriplesLexer(Files.lines(path))
         val parser = NTriplesParser(lexer, handler)
         parser.start()
+    }
+
+    fun createStream(text: String) : Stream<String> {
+        val reader = BufferedReader(StringReader(text))
+        return reader.lines()
     }
 }
