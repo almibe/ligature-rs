4d17b904a29fa1d7f15faef699f5cce29d871691
Alex Berry <berry3@indiana.edu>
2017-06-15 22:14:30 -0400

    deleting old hand written code

--- src/main/kotlin/org/almibe/stinkpot/Token.kt
+++
@@ -1,9 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-package org.almibe.stinkpot
-
-interface TokenType
-
-data class Token<out T: TokenType> (val tokenType:T, val text:String)
