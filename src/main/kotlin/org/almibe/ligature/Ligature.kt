654b0837d7aab43af3ba83901656b4137f609da8
Alex Berry <berry3@indiana.edu>
2017-06-15 22:12:36 -0400

    creating main class with stubs from old code base

--- src/main/kotlin/org/almibe/ligature/Ligature.kt
+++ src/main/kotlin/org/almibe/ligature/Ligature.kt
@@ -2,21 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.almibe.stinkpot
+package org.almibe.ligature
 
-import org.almibe.ligature.Triple
-import org.almibe.stinkpot.ntriples.NTriplesLexer
-import org.almibe.stinkpot.ntriples.NTriplesParser
-import org.almibe.stinkpot.turtle.TurtleLexer
-import org.almibe.stinkpot.turtle.TurtleParser
 import java.io.BufferedReader
 import java.io.StringReader
-import java.nio.file.Files
 import java.nio.file.Path
 import java.util.*
 import java.util.stream.Stream
 
-class Stinkpot {
+class Ligature {
     fun parseNTriples(text: String) : List<Triple>  {
         val triples : ArrayList<Triple> = ArrayList()
         parseNTriples(text) { triples.add(it) }
@@ -24,45 +18,45 @@ class Stinkpot {
     }
 
     fun parseNTriples(text: String, handler: (Triple) -> Unit) {
-        val lexer = NTriplesLexer(createStream(text))
-        val parser = NTriplesParser(lexer, handler)
-        parser.start()
+//        val lexer = NTriplesLexer(createStream(text))
+//        val parser = NTriplesParser(lexer, handler)
+//        parser.start()
     }
 
     fun parseNTriples(path: Path) : List<Triple> {
         val triples : ArrayList<Triple> = ArrayList()
-        parseNTriples(path) { triples.add(it) }
+//        parseNTriples(path) { triples.add(it) }
         return triples
     }
 
     fun parseNTriples(path: Path, handler: (Triple) -> Unit) {
-        val lexer = NTriplesLexer(Files.lines(path))
-        val parser = NTriplesParser(lexer, handler)
-        parser.start()
+//        val lexer = NTriplesLexer(Files.lines(path))
+//        val parser = NTriplesParser(lexer, handler)
+//        parser.start()
     }
 
     fun parseTurtle(text: String) : List<Triple>  {
         val triples : ArrayList<Triple> = ArrayList()
-        parseTurtle(text) { triples.add(it) }
+//        parseTurtle(text) { triples.add(it) }
         return triples
     }
 
     fun parseTurtle(text: String, handler: (Triple) -> Unit) {
-        val lexer = TurtleLexer(createStream(text))
-        val parser = TurtleParser(lexer, handler)
-        parser.start()
+//        val lexer = TurtleLexer(createStream(text))
+//        val parser = TurtleParser(lexer, handler)
+//        parser.start()
     }
 
     fun parseTurtle(path: Path) : List<Triple> {
         val triples : ArrayList<Triple> = ArrayList()
-        parseTurtle(path) { triples.add(it) }
+//        parseTurtle(path) { triples.add(it) }
         return triples
     }
 
     fun parseTurtle(path: Path, handler: (Triple) -> Unit) {
-        val lexer = TurtleLexer(Files.lines(path))
-        val parser = TurtleParser(lexer, handler)
-        parser.start()
+//        val lexer = TurtleLexer(Files.lines(path))
+//        val parser = TurtleParser(lexer, handler)
+//        parser.start()
     }
 
     fun createStream(text: String) : Stream<String> {
