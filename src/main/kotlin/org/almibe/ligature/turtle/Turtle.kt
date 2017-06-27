950caf5a9a861688bf2a3bc244f6bf77071a2fbb
Alex Berry <berry3@indiana.edu>
2017-06-27 17:39:13 -0400

    starting to fill in Turtle impl

--- src/main/kotlin/org/almibe/ligature/turtle/Turtle.kt
+++ src/main/kotlin/org/almibe/ligature/turtle/Turtle.kt
@@ -4,10 +4,52 @@
 
 package org.almibe.ligature.turtle
 
+import org.almibe.ligature.IRI
 import org.almibe.ligature.Triple
+import org.almibe.ligature.parser.TurtleBaseVisitor
+import org.almibe.ligature.parser.TurtleLexer
+import org.almibe.ligature.parser.TurtleParser
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
 
 class Turtle {
+    fun parseTurtle(text: String) : List<Triple> {
+        val parser = TurtleParserInstance()
+        return parser.parseTurtle(text)
+    }
+}
+
+private class TurtleParserInstance {
+    private var base: String = ""
+    private val prefixes: Map<String, String> = mutableMapOf()
+
     fun parseTurtle(text: String) : List<Triple>  {
-        TODO()
+        val stream = CharStreams.fromString(text)
+        val lexer = TurtleLexer(stream)
+        val tokens = CommonTokenStream(lexer)
+        val parser = TurtleParser(tokens)
+
+        val turtleDocVisitor = TurtleDocVisitor()
+        return turtleDocVisitor.visit(parser.turtleDoc())
+    }
+
+
+    inner class TurtleDocVisitor : TurtleBaseVisitor<List<Triple>>() {
+        override fun visitTurtleDoc(ctx: TurtleParser.TurtleDocContext): List<Triple> {
+            val statementVisitor = StatementVisitor()
+            return ctx.statement().map {
+                it.accept(statementVisitor)
+            }
+        }
+    }
+
+    inner class StatementVisitor : TurtleBaseVisitor<Triple>() {
+        override fun visitStatement(ctx: TurtleParser.StatementContext): Triple {
+            return Triple(IRI(""), IRI(""), IRI(""))
+//        val subject = SubjectVisitor().visitSubject(ctx.subject())
+//        val predicate = PredicateVisitor().visitPredicate(ctx.predicate())
+//        val `object` = ObjectVisitor().visitObject(ctx.`object`())
+//        return Triple(subject, predicate, `object`)
+        }
     }
 }
