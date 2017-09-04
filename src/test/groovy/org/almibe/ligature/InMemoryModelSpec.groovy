f49fcd72b78e44f66a6b8b6969ff9448284e0b61
Alex Berry <berry3@indiana.edu>
2017-09-04 17:46:35 -0400

    filling in unimplemented tests

--- src/test/groovy/org/almibe/ligature/InMemoryModelSpec.groovy
+++ src/test/groovy/org/almibe/ligature/InMemoryModelSpec.groovy
@@ -30,15 +30,37 @@ class InMemoryModelSpec extends Specification {
         model.literals == [new LangLiteral("Dr. Octopus", "en"), new LangLiteral("Spiderman", "en")].toSet()
     }
 
-//    def "adding a single subject should be saved without any statements attached to it"() {
-//
-//    }
-//
-//    def "removing a subject should remove all predicates attached to it and all literals attached to those predicates"() {
-//
-//    }
-//
-//    def "removing statements shouldn't remove any subjects just predicates and literals"() {
-//
-//    }
+    def "adding a single subject should be saved without any statements attached to it"() {
+        given:
+        model.addSubject(NTriplesSpec.spiderMan)
+        expect:
+        model.subjects.size() == 1
+        model.statementsFor(NTriplesSpec.spiderMan).size() == 0
+        model.subjects.first() == NTriplesSpec.spiderMan
+    }
+
+    def "removing a subject should remove all predicates attached to it and all literals attached to those predicates"() {
+        given:
+        model.addStatement(NTriplesSpec.spiderMan, NTriplesSpec.enemyOf, NTriplesSpec.greenGoblin)
+        model.addStatement(NTriplesSpec.spiderMan, NTriplesSpec.enemyOf, new LangLiteral("Dr. Octopus", "en"))
+        model.addStatement(NTriplesSpec.greenGoblin, NTriplesSpec.enemyOf, NTriplesSpec.spiderMan)
+        model.addStatement(NTriplesSpec.greenGoblin, new IRI("http://xmlns.com/foaf/0.1/name"), new LangLiteral("Green Goblin", "en"))
+        model.addStatement(NTriplesSpec.spiderMan, new IRI("http://xmlns.com/foaf/0.1/name"), new LangLiteral("Spiderman", "en"))
+        model.removeSubject(NTriplesSpec.greenGoblin)
+        expect:
+        model.subjects == [NTriplesSpec.spiderMan].toSet()
+        model.statementsFor(NTriplesSpec.spiderMan).size() == 2
+    }
+
+    def "removing statements shouldn't remove any subjects just predicates and literals"() {
+        given:
+        model.addStatement(NTriplesSpec.spiderMan, NTriplesSpec.enemyOf, NTriplesSpec.greenGoblin)
+        model.addStatement(NTriplesSpec.spiderMan, NTriplesSpec.enemyOf, new LangLiteral("Dr. Octopus", "en"))
+        model.addStatement(NTriplesSpec.spiderMan, new IRI("http://xmlns.com/foaf/0.1/name"), new LangLiteral("Spiderman", "en"))
+        model.removeStatement(NTriplesSpec.spiderMan, NTriplesSpec.enemyOf, NTriplesSpec.greenGoblin)
+        expect:
+        model.subjects == [NTriplesSpec.spiderMan, NTriplesSpec.greenGoblin].toSet()
+        model.statementsFor(NTriplesSpec.spiderMan).size() == 2
+        model.statementsFor(NTriplesSpec.greenGoblin).size() == 0
+    }
 }
