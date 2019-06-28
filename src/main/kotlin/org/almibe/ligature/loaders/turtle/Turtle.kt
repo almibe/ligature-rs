b2ed1d716d56bc3d4c2eadefc5b9ebe3cf8afd75
Alex Michael Berry <berry3@indiana.edu>
2019-06-27 20:18:15 -0400

    removing Ligature class, starting to add Loader interface

--- src/main/kotlin/org/almibe/ligature/loaders/turtle/Turtle.kt
+++ src/main/kotlin/org/almibe/ligature/loaders/turtle/Turtle.kt
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.almibe.ligature.loaders
+package org.almibe.ligature.loaders.turtle
 
 import org.almibe.ligature.*
 import org.almibe.ligature.parser.turtle.ModalTurtleLexer
