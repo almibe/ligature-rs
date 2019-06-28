1dc38ae34c5f4b68736a04a5ceb094378440ac73
Alex Michael Berry <berry3@indiana.edu>
2019-06-28 00:19:18 -0400

    minor clean up

--- src/main/kotlin/org/almibe/ligature/RdfModel.kt
+++ src/main/kotlin/org/almibe/ligature/RdfModel.kt
@@ -29,29 +29,25 @@ interface Graph {
     fun getSubjects(): Stream<Subject>
 
     /**
-     * Adds the passed in model to this current one.  IRIs are merged and all blank nodes from the new model are
-     * given unique names in the current model.  Literals are not merged.
+     * Adds the passed in graph to this current one.  IRIs are merged and all blank nodes from the new model are
+     * given unique names in the current graph.  Literals are not merged.
      */
-    fun addModel(graph: Graph)
+    fun addGraph(graph: Graph)
     /**
-     * Add a given statement to this model.  The subject will be added if it doesn't already exist, a new predicate
+     * Add a given statement to this graph.  The subject will be added if it doesn't already exist, a new predicate
      * is always added to this subject, and the object is always created if it is a Literal otherwise a new object
      * is only created if it doesn't already exist.
      */
     fun addStatement(subject: Subject, predicate: Predicate, `object`: Object)
     /**
-     * Remove a given statement from this model.  The subject is not removed, the predicate always is removed,
+     * Remove a given statement from this graph.  The subject is not removed, the predicate always is removed,
      * and the object is removed only if it is a Literal.
      */
     fun removeStatement(subject: Subject, predicate: Predicate, `object`: Object)
     /**
-     * Remove a subject from this model.  All predicates related to this object are removed and all objects of those
+     * Remove a subject from this graph.  All predicates related to this object are removed and all objects of those
      * statements are removed if they are Literals, BlankNodes and IRIs are left in the model.  This method does
      * nothing if the subject doesn't exist.
      */
     fun removeSubject(subject: Subject)
-    /**
-     * Adds a new subject to this model or does nothing if this subject already exists.
-     */
-    fun addSubject(subject: Subject)
 }
