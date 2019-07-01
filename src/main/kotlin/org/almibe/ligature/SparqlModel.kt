4675e3dcf7da9e2b8a1f9f5fbd1c35ef20344513
Alex Michael Berry <berry3@indiana.edu>
2019-06-30 23:09:41 -0400

    starting to fill in SPARQL model

--- src/main/kotlin/org/almibe/ligature/SparqlModel.kt
+++ src/main/kotlin/org/almibe/ligature/SparqlModel.kt
@@ -6,3 +6,48 @@ package org.almibe.ligature
 
 sealed class SparqlResult
 sealed class SparqlCommand
+
+data class Query(val queryType: QueryType, val valuesClause ValuesClause): SparqlCommand
+data class Update(TODO): SparqlCommand
+
+enum SelectModifier { DISTINCT, REDUCED }
+data class Var(name: String, as: String?)
+
+sealed class QueryType
+data class SelectQuery(val selectClause: SelectClause, val datasetClauses: List<DatasetClause>, val whereClause: WhereClause, val solutionModifier: SolutionModifier): QueryType
+data class ConstructQuery(TODO): QueryType
+data class DescribeQuery(TODO): QueryType
+data class AskQuery(TODO): QueryType
+
+data class SelectClause(val modifier: SelectModifier?, val vars: List<Var>)
+
+sealed class DatasetClause
+data class DefaultGraphClause(val sourceSelector: IRI): DatasetClause
+data class NamedGraphClause(val sourceSelector: IRI): DatasetClause
+
+data class WhereClause(val groupGraphPattern: GroupGraphPattern)
+
+sealed class GroupGraphPattern
+data class SubSelect(val selectClause: SelectClause, val whereClause: WhereClause, val solutionModifier: SolutionModifier, val valuesClause: ValuesClause): GroupGraphPattern
+data class GroupGraphPatternSub(TODO): GroupGraphPattern
+
+data class SolutionModifier(val groupClause: GroupClause?, val havingClause: HavingClause?, val orderClause: OrderClause?, val limitOffsetClauses: LimitOffsetClauses?)
+
+data class GroupClause(val groupConditions: List<GroupCondition>)
+data class HavingClause(val constraint: Constraint)
+data class OrderClause(val orderConditions: List<OrderCondition>)
+
+sealed class LimitOffsetClauses
+data class LimitOffsetClause(val limit: Int, val offset: Int?): LimitOffsetClauses
+data class OffsetLimitClause(val offset: Int, val limit: Int?): LimitOffsetClauses
+
+sealed class Constraint
+data class BrackettedExpression(TODO): Constraint
+data class BuiltInCall(TODO): Constraint
+data class FunctionCall(TODO): Constraint
+
+data class ValuesClause(val dataBlock: DataBlock?)
+
+sealed class DataBlock
+data class InlineDataOneVar(TODO): DataBlock
+data class InlineDataFull(TODO): DataBlock
