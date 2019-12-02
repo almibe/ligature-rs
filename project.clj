c880764b0ce9d034678b45298fbd23984ee7c87b
Alex Michael Berry <berry3@indiana.edu>
2019-12-02 01:23:36 -0500

    basic lein build, closes #57

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ligature "0.1.0-SNAPSHOT"
+  :description "Ligature is an rdf library for Clojure"
+  :url "https://github.com/almibe/ligature"
+  :license {:name "MPL-2.0"
+            :url "https://www.mozilla.org/en-US/MPL/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns ligature.core})
---
+++ src/ligature/core.clj
@@ -0,0 +1,6 @@
+(ns ligature.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
