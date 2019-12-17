319af2a224cb2dea47d6bdaaa746c8511624f923
Alex Michael Berry <berry3@indiana.edu>
2019-12-16 23:38:25 -0500

    filling in predicates

--- src/org/almibe/ligature/core.clj
+++ src/org/almibe/ligature/core.clj
@@ -31,19 +31,36 @@
 (defn identifier?
   "Accepts a String representing an identifier and returns true or false depending on if it is valid."
   [identifier]
-  (throw (RuntimeException. "TODO")))
+  (and
+    (string? identifier)
+    (not (nil?(re-matches #"[a-zA-Z_][^\s\(\)\[\]\{\}\'\"`<>\\]*" identifier)))))
+
+(defn lang-tag?
+  "Accepts a String representing a lang tag and returns true or false depending on if it is valid."
+  [lang]
+  (and
+    (string? lang)
+    (not (nil?(re-matches #"[a-zA-Z]+(-[a-zA-Z0-9]+)*" lang)))))
 
 (defn lang-literal?
   "Accepts a Map and returns true or false depending on if it is a valid lang literal.
   A lang literal should contain a :value key with a valid string literal and a :lang key with a valid lang code."
   [literal]
-  (throw (RuntimeException. "TODO")))
+  (and
+    (map? literal)
+    (= (keys literal) [:lang :value])
+    (lang-tag? (:lang literal))
+    (string? (:value literal))))
 
 (defn typed-literal?
   "Accepts a Map and returns true or false depending on if it is a valid typed literal.
   A typed literal should contain a :valud key with a valid string literal and a :type key with a valid identifier."
   [literal]
-  (throw (RuntimeException. "TODO")))
+  (and
+    (map? literal)
+    (= (keys literal) [:type :value])
+    (identifier? (:type literal))
+    (string? (:value literal))))
 
 (defn literal?
   "Accepts a String or Map representing a literal and returns true or false depending on if it is valid."
@@ -64,11 +81,26 @@
   "Accepts a String or Map representing an object and returns true or false depending on if it is valid."
   (or (identifier? object) (literal? object)))
 
+(defn graph?
+  "Checks that a passed String value is either a valid identifier or nil"
+  [graph]
+  (or
+    (nil? graph)
+    (identifier? graph)))
+
 (defn statement?
   "Accepts a Map and returns true or false depending on if it is a valid Statement.
   A valid Statement contains a :subject, :predicate, :object, and optionally a :graph key with valid values."
   [statement]
-  (throw (RuntimeException. "TODO")))
+  (and
+    (map? statement)
+    (or
+      (= (keys statement) [:subject :predicate :object])
+      (= (keys statement) [:subject :predicate :object :graph]))
+    (subject? (:subject statement))
+    (predicate? (:predicate statement))
+    (object? (:object statement))
+    (graph? (:graph statement))))
 
 (defn statement
   "This function acts as a helper function for creating Statement maps."
