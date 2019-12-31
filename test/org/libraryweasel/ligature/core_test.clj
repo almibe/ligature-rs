2464ec3ed5c0f9c2e1cf86771cc49203dedac00f
Alex Michael Berry <berry3@indiana.edu>
2019-12-31 18:51:40 -0500

    adding extra test for :a

--- test/org/libraryweasel/ligature/core_test.clj
+++ test/org/libraryweasel/ligature/core_test.clj
@@ -42,7 +42,8 @@
     (is (statement? (statement "hello" "world" "triple" "graph")))
     (is (not (statement? (statement 5 3 66 554))))
     (is (not (statement? (statement "test" "test" :a))))
-    (is (= (statement "test" :a "test" "test") (statement "test" "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" "test" "test"))))) ; TODO more test cases
+    (is (= (statement "test" :a "test" "test") (statement "test" "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" "test" "test")))
+    (is (statement? (statement "test" :a "test" "test"))))) ; TODO more test cases
 
 (deftest lang-tag?-test
   (testing "Common examples"
