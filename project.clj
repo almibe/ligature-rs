ab3017a6631409d229fca104b3ed84efde1279e2
Alex Michael Berry <berry3@indiana.edu>
2020-02-04 01:15:53 -0500

    Removing project.clj, closes #73

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject org.libraryweasel/ligature "0.1.0-SNAPSHOT"
-  :description "Ligature is a Clojure library for working with semantic data."
-  :url "https://github.com/almibe/ligature"
-  :license {:name "EPL-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns org.libraryweasel.ligature.core})
