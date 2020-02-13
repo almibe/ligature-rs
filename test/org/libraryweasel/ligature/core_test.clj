9166fc96603724df64671260a92712a1eeb4213d
Alex Michael Berry <berry3@indiana.edu>
2020-02-13 08:02:46 -0500

    Fixing imports

--- test/org/libraryweasel/ligature/core_test.clj
+++ test/org/libraryweasel/ligature/core_test.clj
@@ -8,35 +8,34 @@
 
 (ns org.libraryweasel.ligature.core-test
   (:require [clojure.test :refer :all]
-            [org.libraryweasel.ligature.core :refer :all]
-            [clojure.spec.alpha :as s]
-            [org.libraryweasel.ligature.core :as l]))
+            [org.libraryweasel.ligature.core :as l]
+            [clojure.spec.alpha :as s]))
 
 (deftest identifier?-test
   (testing "Common examples"
-    (is (not (identifier? "")))
-    (is (identifier? "http://localhost/people/7"))
-    (is (not (identifier? "http://localhost(/people/7")))
-    (is (not (identifier? "http://localhost /people/7")))
-    (is (identifier? "hello"))
-    (is (identifier? "_:"))
-    (is (identifier? "_:valid"))
-    (is (identifier? "_:1"))
-    (is (identifier? "_:1344")))) ; TODO more test cases
+    (is (not (l/identifier? "")))
+    (is (l/identifier? "http://localhost/people/7"))
+    (is (not (l/identifier? "http://localhost(/people/7")))
+    (is (not (l/identifier? "http://localhost /people/7")))
+    (is (l/identifier? "hello"))
+    (is (l/identifier? "_:"))
+    (is (l/identifier? "_:valid"))
+    (is (l/identifier? "_:1"))
+    (is (l/identifier? "_:1344")))) ; TODO more test cases
 
 (deftest lang-literal?-test
   (testing "Common examples"
-    (is (not (lang-literal? "not a lang lit")))
-    (is (not (lang-literal? {:value "" :lang ""})))
-    (is (lang-literal? {:value "Hello" :lang "en"}))
-    (is (not (lang-literal? {:value "Bonjour" :lang "fr" :type "fr"}))))) ; TODO more test cases
+    (is (not (l/lang-literal? "not a lang lit")))
+    (is (not (l/lang-literal? {:value "" :lang ""})))
+    (is (l/lang-literal? {:value "Hello" :lang "en"}))
+    (is (not (l/lang-literal? {:value "Bonjour" :lang "fr" :type "fr"}))))) ; TODO more test cases
 
 (deftest typed-literal?-test
   (testing "Common examples"
-    (is (not (typed-literal? "not a typed literal")))
-    (is (not (typed-literal? {})))
-    (is (typed-literal? {:value "Hello" :type "identifier"}))
-    (is (not (typed-literal? {:value "56" :type "number" :lang "en"}))))) ; TODO more test cases
+    (is (not (l/typed-literal? "not a typed literal")))
+    (is (not (l/typed-literal? {})))
+    (is (l/typed-literal? {:value "Hello" :type "identifier"}))
+    (is (not (l/typed-literal? {:value "56" :type "number" :lang "en"}))))) ; TODO more test cases
 
 (deftest statement?-test
   (testing "Common examples"
@@ -58,11 +57,11 @@
 
 (deftest lang-tag?-test
   (testing "Common examples"
-    (is (not (lang-tag? "")))
-    (is (lang-tag? "en"))
-    (is (not (lang-tag? "en-")))
-    (is (lang-tag? "en-fr"))
-    (is (not (lang-tag? "en-fr-")))
-    (is (lang-tag? "en-fr-sp"))
-    (is (lang-tag? "ennnenefnk-dkfjkjfl-dfakjelfkjalkf-fakjeflkajlkfj"))
-    (is (not (lang-tag? "en-fr-ef ")))))
+    (is (not (l/lang-tag? "")))
+    (is (l/lang-tag? "en"))
+    (is (not (l/lang-tag? "en-")))
+    (is (l/lang-tag? "en-fr"))
+    (is (not (l/lang-tag? "en-fr-")))
+    (is (l/lang-tag? "en-fr-sp"))
+    (is (l/lang-tag? "ennnenefnk-dkfjkjfl-dfakjelfkjalkf-fakjeflkajlkfj"))
+    (is (not (l/lang-tag? "en-fr-ef ")))))
