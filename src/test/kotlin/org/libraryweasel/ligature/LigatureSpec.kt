28e7dc7a5570b82fb90c810f2bb450e3c684f5d9
Alex Michael Berry <berry3@indiana.edu>
2020-03-26 18:10:22 -0400

    Reverting to Kotlin

--- src/test/kotlin/org/libraryweasel/ligature/LigatureSpec.kt
+++ src/test/kotlin/org/libraryweasel/ligature/LigatureSpec.kt
@@ -2,7 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/*
+package org.libraryweasel.ligature
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
 class LigatureSpec : StringSpec({
     "validIdentifier tests" {
         validIdentifier("") shouldBe false
@@ -27,4 +31,3 @@ class LigatureSpec : StringSpec({
         validLangTag("en-fr-ef ") shouldBe false
     }
 })
-*/
