5a2ff6132c56299062eced967f2efea54c6ec005
Alex Michael Berry <berry3@indiana.edu>
2020-03-30 21:23:40 -0400

    updating api so all reads and write take place in a transaction, #96

--- src/main/kotlin/org/libraryweasel/ligature/Ligature.kt
+++ src/main/kotlin/org/libraryweasel/ligature/Ligature.kt
@@ -47,27 +47,8 @@ data class LongLiteralRange(override val start: Long, override val end: Long): R
 data class DoubleLiteralRange(override val start: Double, override val end: Double): Range<Double>(start, end)
 
 interface LigatureStore {
-    /**
-     * Returns a collection based on the name passed.
-     * Calling this function will not create a new collection, it just binds a Store and Collection name.
-     */
-    fun collection(collectionName: Entity): LigatureCollection
-
-    /**
-     * Creates a new collection or does nothing if collection already exists.
-     * Regardless the collection is returned.
-     */
-    fun createCollection(collectionName: Entity): LigatureCollection
-
-    /**
-     * Deletes the collection of the name given and does nothing if the collection doesn't exist.
-     */
-    fun deleteCollection(collectionName: Entity)
-
-    /**
-     * Returns a Flow of all existing collections.
-     */
-    fun allCollections(): Flow<Entity>
+    fun readTx(): ReadTx
+    fun writeTx(): WriteTx
 
     /**
      * Close connection with the Store.
@@ -75,16 +56,44 @@ interface LigatureStore {
     fun close()
 }
 
-/**
- * Manages a collection of Statements and Rules, supports ontologies, and querying.
- */
-interface LigatureCollection {
-    val collectionName: Entity
-    fun readTx(): ReadTx
-    fun writeTx(): WriteTx
+interface ReadTx {
+    /**
+     * Returns a Flow of all existing collections.
+     */
+    fun allCollections(): Flow<Entity>
+
+    /**
+     * Returns a collection based on the name passed.
+     * Calling this function will not create a new collection, it just binds a Store and Collection name.
+     */
+    fun collectionReadTx(collectionName: Entity): CollectionReadTx
+
+    /**
+     * Cancels this transaction.
+     */
+    fun cancel()
 }
 
-interface ReadTx {
+interface WriteTx: ReadTx {
+    /**
+     * Deletes the collection of the name given and does nothing if the collection doesn't exist.
+     */
+    fun deleteCollection(collectionName: Entity)
+
+    /**
+     * Creates a new collection or does nothing if collection already exists.
+     * Regardless the collection is returned.
+     */
+    fun collectionWriteTx(collectionName: Entity): CollectionWriteTx
+
+    /**
+     * Commits this transaction.
+     */
+    fun commit()
+}
+
+interface CollectionReadTx {
+    val collectionName: Entity
     /**
      * Accepts nothing but returns a Flow of all Statements in the Collection.
      */
@@ -99,21 +108,15 @@ interface ReadTx {
      * Is passed a pattern and returns a seq with all matching Statements.
      */
     fun matchStatements(subject: Entity? = null, predicate: Predicate? = null, range: Range<*>, context: Entity? = null): Flow<Statement>
-
-    /**
-     * Cancels this transaction.
-     */
-    fun cancel()
 }
 
-interface WriteTx: ReadTx {
+interface CollectionWriteTx: CollectionReadTx {
     /**
      * Returns a new, unique to this collection identifier in the form _:NUMBER"
      */
     fun newEntity(): Entity
     fun addStatement(statement: Statement)
     fun removeStatement(statement: Statement)
-    fun commit()
 }
 
 /**
