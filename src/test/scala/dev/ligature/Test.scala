4d0055b2469d5ecec01b2c814b5239911588ee96
Alex Michael Berry <berry3@indiana.edu>
2020-06-02 00:09:57 -0400

    Commiting updated .gitignore and test file

--- src/test/scala/dev/ligature/Test.scala
+++ src/test/scala/dev/ligature/Test.scala
@@ -2,7 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- import org.junit.Test
+package dev.ligature
+
+import org.junit.Test
 import org.junit.Assert._
 
 class Test {
@@ -10,3 +12,28 @@ class Test {
     assertEquals("I was compiled by dotty :)", Main.msg)
   }
 }
+
+class LigatureSpec : StringSpec({
+    "validIdentifier tests" {
+        validPredicate("") shouldBe false
+        validPredicate("http://localhost/people/7") shouldBe true
+        validPredicate("http://localhost(/people/7") shouldBe false
+        validPredicate("http://localhost /people/7") shouldBe false
+        validPredicate("hello") shouldBe true
+        validPredicate("_:") shouldBe true
+        validPredicate("_:valid") shouldBe true
+        validPredicate("_:1") shouldBe true
+        validPredicate("_:1344") shouldBe true
+    }
+
+    "validLangTag tests" {
+        validLangTag("") shouldBe false
+        validLangTag("en") shouldBe true
+        validLangTag("en-") shouldBe false
+        validLangTag("en-fr") shouldBe true
+        validLangTag("en-fr-") shouldBe false
+        validLangTag("en-fr-sp") shouldBe true
+        validLangTag("ennnenefnk-dkfjkjfl-dfakjelfkjalkf-fakjeflkajlkfj") shouldBe true
+        validLangTag("en-fr-ef ") shouldBe false
+    }
+})
