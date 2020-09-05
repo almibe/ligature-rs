fb540fa11a52c581e2c444ffb18ace974ca820f6
berry3 <berry3@indiana.edu>
2020-09-05 18:02:27 -0400

    starting to add fs2 + cats effect, #138

--- src/main/scala/dev/ligature/Ligature.scala
+++ src/main/scala/dev/ligature/Ligature.scala
@@ -4,6 +4,8 @@
 
 package dev.ligature
 
+import cats.effect.{IO, Resource}
+
 sealed trait Object
 sealed trait Node extends Object
 sealed trait NamedNode extends Node
@@ -17,7 +19,7 @@ case class BooleanLiteral(val value: Boolean) extends Literal()
 case class LongLiteral(val value: Long) extends Literal()
 case class DoubleLiteral(val value: Double) extends Literal()
 
-val a: IRIElement = IRIElement("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+val a: IRINode = IRINode("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
 
 /**
  * Accepts a String representing an identifier and returns true or false depending on if it is valid.
@@ -34,83 +36,80 @@ def validLangTag(langTag: String): Boolean =
 case class Statement(val subject: Node, val predicate: NamedNode, val `object`: Object)
 case class PersistedStatement(val collection: LocalNode, val statement: Statement, val context: AnonymousNode)
 
-//trait Ligature
-//  def use(fn: (LigatureStore) -> Unit)
-//
-//trait LigatureStore {
-//suspend fun <T>read(fn: (ReadTx)->T): T
-//suspend fun write(fn: (WriteTx)->Unit)
-//}
-//
-//trait ReadTx {
-///**
-// * Returns a Iterable of all existing collections.
-// */
-//suspend fun collections(): Flow<NamedElement>
-//
-///**
-// * Returns a Iterable of all existing collections that start with the given prefix.
-// */
-//suspend fun collections(prefix: NamedElement): Flow<NamedElement>
-//
-///**
-// * Returns a Iterable of all existing collections that are within the given range.
-// * `from` is inclusive and `to` is exclusive.
-// */
-//suspend fun collections(from: NamedElement, to: NamedElement): Flow<NamedElement>
-//
-///**
-// * Accepts nothing but returns a Iterable of all Statements in the Collection.
-// */
-//suspend fun allStatements(collection: NamedElement): Flow<PersistedStatement>
-//
-///**
-// * Is passed a pattern and returns a seq with all matching Statements.
-// */
-//suspend fun matchStatements(collection: NamedElement,
-//subject: Option<Subject> = None,
-//predicate: Option<NamedElement> = None,
-//`object`: Option<Element> = None): Flow<PersistedStatement>
-//
-////  /**
-////   * Is passed a pattern and returns a seq with all matching Statements.
-////   */
-////  fun matchStatements(collection: NamedEntity,
-////                      subject: Option<Entity>,
-////                      predicate: Option<Predicate>,
-////                      range: ClosedRange<RangeLiteral>): Any, Throwable, Iterable<PersistedStatement>>
-//
-///**
-// * Returns the Statement with the given context.
-// * Returns None if the context doesn't exist.
-// */
-//suspend fun statementByContext(collection: NamedElement, context: AnonymousElement): Option<PersistedStatement>
-//}
-//
-//trait WriteTx {
-///**
-// * Creates a collection with the given name or does nothing if the collection already exists.
-// * Only useful for creating an empty collection.
-// */
-//suspend fun createCollection(collection: NamedElement): NamedElement
-//
-///**
-// * Deletes the collection of the name given and does nothing if the collection doesn't exist.
-// */
-//suspend fun deleteCollection(collection: NamedElement): NamedElement
-//
-///**
-// * Returns a new, unique to this collection, AnonymousEntity
-// */
-//suspend fun newEntity(collection: NamedElement): AnonymousElement
-//suspend fun addStatement(collection: NamedElement, statement: Statement): PersistedStatement
-////  Commenting out the below as part of #125
-////  fun removeStatement(collection: NamedEntity, statement: Statement): Any, Throwable, Statement>>
-////  fun removeEntity(collection: NamedEntity, entity: Entity): Any, Throwable, Entity>>
-////  fun removePredicate(collection: NamedEntity, predicate: Predicate): Any, Throwable, Predicate>>
-//
-///**
-// * Cancels this transaction.
-// */
-//suspend fun cancel()
-//}
+trait Ligature
+  def store(): Resource[IO, LigatureStore]
+
+trait LigatureStore
+  def read(): Resource[IO, ReadTx]
+  def write(): Resource[IO, WriteTx]
+
+trait ReadTx
+  /**
+   * Returns a Iterable of all existing collections.
+   */
+  def collections(): Stream[IO, NamedElement]
+
+  /**
+   * Returns a Iterable of all existing collections that start with the given prefix.
+   */
+  def collections(prefix: NamedElement): Stream[IO, NamedElement]
+
+  /**
+   * Returns a Iterable of all existing collections that are within the given range.
+   * `from` is inclusive and `to` is exclusive.
+   */
+  def collections(from: NamedElement, to: NamedElement): Stream[IO, NamedElement]
+
+  /**
+   * Accepts nothing but returns a Iterable of all Statements in the Collection.
+   */
+  def allStatements(collection: NamedElement): Stream[IO, PersistedStatement]
+
+  /**
+   * Is passed a pattern and returns a seq with all matching Statements.
+   */
+  def matchStatements(collection: NamedElement,
+    subject: Option<Subject> = None,
+    predicate: Option<NamedElement> = None,
+    `object`: Option<Element> = None): Stream[IO, PersistedStatement]
+
+//  /**
+//   * Is passed a pattern and returns a seq with all matching Statements.
+//   */
+//  fun matchStatements(collection: NamedEntity,
+//                      subject: Option<Entity>,
+//                      predicate: Option<Predicate>,
+//                      range: ClosedRange<RangeLiteral>): Any, Throwable, Iterable<PersistedStatement>>
+
+  /**
+   * Returns the Statement with the given context.
+   * Returns None if the context doesn't exist.
+   */
+  def statementByContext(collection: NamedElement, context: AnonymousElement): IO[Option[PersistedStatement]]
+
+trait WriteTx {
+  /**
+   * Creates a collection with the given name or does nothing if the collection already exists.
+   * Only useful for creating an empty collection.
+   */
+  def createCollection(collection: NamedElement): IO[NamedElement]
+
+  /**
+   * Deletes the collection of the name given and does nothing if the collection doesn't exist.
+   */
+  def deleteCollection(collection: NamedElement): IO[NamedElement]
+
+  /**
+   * Returns a new, unique to this collection, AnonymousEntity
+   */
+  def newEntity(collection: NamedElement): IO[AnonymousElement]
+  def addStatement(collection: NamedElement, statement: Statement): IO[PersistedStatement]
+  //  Commenting out the below as part of #125
+  //  fun removeStatement(collection: NamedEntity, statement: Statement): Any, Throwable, Statement>>
+  //  fun removeEntity(collection: NamedEntity, entity: Entity): Any, Throwable, Entity>>
+  //  fun removePredicate(collection: NamedEntity, predicate: Predicate): Any, Throwable, Predicate>>
+
+  /**
+   * Cancels this transaction.
+   */
+  def cancel()
