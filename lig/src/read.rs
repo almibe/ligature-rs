a291bcc8e5fb854a12c05c78c46f9c2b903d0542
berry3 <berry3@indiana.edu>
2021-09-06 17:10:21 -0400

    starting to use gaze for lig

--- lig/src/read.rs
+++ lig/src/read.rs
@@ -4,23 +4,37 @@
 
 use crate::LigError;
 use ligature::{Attribute, Bytes, Entity, Statement, Value};
+use gaze::{Gaze, Step, GazeErr};
+use std::collections::HashSet;
+use gaze::steps::{TakeString, TakeWhile};
 
 /// Reads an Entity from the given &str.
 /// Will return an error if there is anything other than an Entity + whitespace in the input.
 pub fn read_entity(input: &str) -> Result<Entity, LigError> {
-    todo!()
+    let mut gaze = Gaze::new(input.trim());
+    gaze.run(&TakeString("<".into()));
+    let res = gaze.run(&TakeWhile(&|c: &char| c.is_alphabetic()));
+    gaze.run(&TakeString(">".into()));
+    match res {
+        Ok(st) => {
+            Ok(Entity::new(&st)?)
+        }
+        Err(_) => {
+            Err(LigError("Couldn't parse Entity.".into()))
+        }
+    }
 }
 
-pub fn read_attribute(attribute: &str) -> Result<Attribute, LigError> {
+pub fn read_attribute(input: &str) -> Result<Attribute, LigError> {
     todo!()
 }
 
 /// Reads a value from a passed str.
 /// Ignores white space but will return an Err if there is any input besides an encoded value.
-pub fn read_value(value: &str) -> Result<Value, LigError> {
+pub fn read_value(input: &str) -> Result<Value, LigError> {
     todo!()
 }
 
-pub fn read(statements: &str) -> Result<Vec<Statement>, LigError> {
+pub fn read(input: &str) -> Result<Vec<Statement>, LigError> {
     todo!()
 }
