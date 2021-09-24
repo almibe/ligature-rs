a66529c8bc13a0c20747214e8d1957e228f422a9
berry3 <berry3@indiana.edu>
2021-09-23 22:22:37 -0400

    starting work on using new gaze api, #201

--- lig/README.md
+++ lig/README.md
@@ -1,3 +1,5 @@
-#lig
+# lig
 
 A Rust implementation of the lig serialization format for Ligature.
+
+See https://github.com/almibe/ligature-specification/blob/main/lig.md for an explanation of the format.
--- lig/src/read.rs
+++ lig/src/read.rs
@@ -1,100 +1,79 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// // This Source Code Form is subject to the terms of the Mozilla Public
+// // License, v. 2.0. If a copy of the MPL was not distributed with this
+// // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 use crate::LigError;
-use gaze::steps::{TakeString, TakeWhile};
-use gaze::{Gaze, Step};
 use ligature::{validate_identifier_characters, Attribute, Bytes, Entity, Statement, Value};
+use gaze::Gaze;
+use gaze::steps::{NoMatch, take_string, take_while_str};
 
-enum LigToken {
+#[derive(Debug, Clone, Copy)]
+pub enum LigToken {
     OpenAngle,
     CloseAngle,
     AtSign,
-    Identifier(String),
     Whitespace,
     NewLine,
     DoubleQuote,
-    StringContent(String),
-    Integer(String),
     Dot,
+    Identifier,
+    Integer,
 }
 
 /// Reads an Entity from the given &str.
 /// Will return an error if there is anything other than an Entity + whitespace in the input.
 pub fn read_entity(input: &str) -> Result<Entity, LigError> {
-    let mut gaze = Gaze::new(input.trim());
-    let step = EntityStep();
-    gaze.run(&step)
+    let mut gaze = Gaze::<&str>::from_str(input.trim());
+    gaze.attempt(&entity_step).map_err(|_| LigError("Could not read Entity.".into()))
 }
 
 pub fn read_attribute(input: &str) -> Result<Attribute, LigError> {
-    let mut gaze = Gaze::new(input.trim());
-    let step = AttributeStep();
-    gaze.run(&step)
+    let mut gaze = Gaze::<&str>::from_str(input.trim());
+    gaze.attempt(&attribute_step).map_err(|_| LigError("Could not read Attribute.".into()))
 }
 
 /// Reads a value from a passed str.
 /// Ignores white space but will return an Err if there is any input besides an encoded value.
 pub fn read_value(input: &str) -> Result<Value, LigError> {
-    let mut gaze = Gaze::new(input.trim());
-    let step = ValueStep();
-    gaze.run(&step)
+    let mut gaze = Gaze::<&str>::from_str(input.trim());
+    gaze.attempt(&value_step).map_err(|_| LigError("Could not read Value.".into()))
 }
 
 pub fn read(input: &str) -> Result<Vec<Statement>, LigError> {
     todo!()
 }
 
-pub struct EntityStep();
-impl Step<Entity, LigError> for EntityStep {
-    fn attempt(&self, gaze: &mut Gaze) -> Result<Entity, LigError> {
-        gaze.run(&TakeString::new("<"))
-            .map_err(|_| LigError("Could not read Entity.".into()))?;
-        let res = gaze.run(&TakeWhile(&|c: &str| validate_identifier_characters(c)));
-        gaze.run(&TakeString::new(">"))
-            .map_err(|_| LigError("Could not read Entity.".into()))?;
-        match res {
-            Ok(st) => Ok(Entity::new(&st)?),
-            Err(_) => Err(LigError("Couldn't parse Entity.".into())),
-        }
-    }
+fn entity_step(gaze: &mut Gaze<&str>) -> Result<Entity, NoMatch> {
+    gaze.attempt(&take_string("<"))?;//.map_err(|_| LigError("Could not read Entity.".into()))?;
+    let res = gaze.attempt(&take_while_str(&|c: &str| validate_identifier_characters(c)))?;
+    gaze.attempt(&take_string(">"))?;//.map_err(|_| LigError("Could not read Entity.".into()))?;
+    Ok(Entity::new(&res).map_err(|_| NoMatch)?)
 }
 
-pub struct AttributeStep();
-impl Step<Attribute, LigError> for AttributeStep {
-    fn attempt(&self, gaze: &mut Gaze) -> Result<Attribute, LigError> {
-        gaze.run(&TakeString::new("@<"))
-            .map_err(|_| LigError("Could not read Attribute.".into()))?;
-        let res = gaze.run(&TakeWhile(&|c: &str| validate_identifier_characters(c)));
-        gaze.run(&TakeString::new(">"))
-            .map_err(|_| LigError("Could not read Attribute.".into()))?;
-        match res {
-            Ok(st) => Ok(Attribute::new(&st)?),
-            Err(_) => Err(LigError("Couldn't parse Attribute.".into())),
-        }
-    }
+fn attribute_step(gaze: &mut Gaze<&str>) -> Result<Attribute, NoMatch> {
+    gaze.attempt(&take_string("@<"))?;//.map_err(|_| LigError("Could not read Attribute.".into()))?;
+    let res = gaze.attempt(&take_while_str(&|c: &str| validate_identifier_characters(c)))?;
+    gaze.attempt(&take_string(">"))?;//.map_err(|_| LigError("Could not read Attribute.".into()))?;
+    Ok(Attribute::new(&res).map_err(|_| NoMatch)?)
 }
 
-pub struct ValueStep();
-impl Step<Value, LigError> for ValueStep {
-    fn attempt(&self, gaze: &mut Gaze) -> Result<Value, LigError> {
-        let next_char = gaze.peek();
-        return match next_char {
-            None => Err(LigError("Could not match Value".into())),
-            Some(c) => {
-                if c == "<" {
-                    Ok(Value::Entity(gaze.run(&EntityStep())?))
-                } else if is_digit(&c) {
-                    gaze.run(&NumberStep())
-                } else if c == "\"" {
-                    gaze.run(&StringStep())
-                } else {
-                    Err(LigError("Could not match Value".into()))
-                }
-            }
-        };
-    }
+fn value_step(gaze: &mut Gaze<&str>) -> Result<Value, NoMatch> {
+    // let next_char = gaze.peek();
+    // return match next_char {
+    //     None => Err(LigError("Could not match Value".into())),
+    //     Some(c) => {
+    //         if c == "<" {
+    //             Ok(Value::Entity(gaze.run(&EntityTokenizer())?))
+    //         } else if is_digit(&c) {
+    //             gaze.run(&NumberTokenizer())
+    //         } else if c == "\"" {
+    //             gaze.run(&StringTokenizer())
+    //         } else {
+    //             Err(LigError("Could not match Value".into()))
+    //         }
+    //     }
+    // };
+    todo!()
 }
 
 fn is_digit(s: &str) -> bool {
@@ -110,61 +89,61 @@ fn is_digit(s: &str) -> bool {
         || s == "9"
 }
 
-pub struct NumberStep();
-impl Step<Value, LigError> for NumberStep {
-    fn attempt(&self, gaze: &mut Gaze) -> Result<Value, LigError> {
-        let is_hex = gaze.run(&TakeString::new("0x"));
-        match is_hex {
-            Ok(_) => {
-                return gaze.run(&HexStep());
-            },
-            Err(_) => {
-                let integer = gaze.run(&IntegerStep())?;
-                let is_float = gaze.run(&TakeString::new("."));
-                match is_float {
-                    Ok(_) => todo!(),
-                    Err(_) => todo!(),
-                }
-                //if not read integer
-                //then check for decimal point
-                //handle float if decimal point exists
-            },
-        }
-        todo!()
-    }
-}
+// pub struct NumberTokenizer();
+// impl Tokenizer<Value, LigError> for NumberTokenizer {
+//     fn attempt(&self, gaze: &mut Gaze) -> Result<Value, LigError> {
+//         let is_hex = gaze.run(&TakeString::new("0x"));
+//         match is_hex {
+//             Ok(_) => {
+//                 return gaze.run(&HexTokenizer());
+//             },
+//             Err(_) => {
+//                 let integer = gaze.run(&IntegerTokenizer())?;
+//                 let is_float = gaze.run(&TakeString::new("."));
+//                 match is_float {
+//                     Ok(_) => todo!(),
+//                     Err(_) => todo!(),
+//                 }
+//                 //if not read integer
+//                 //then check for decimal point
+//                 //handle float if decimal point exists
+//             },
+//         }
+//         todo!()
+//     }
+// }
 
-pub struct HexStep();
-impl Step<Value, LigError> for HexStep {
-    fn attempt(&self, gaze: &mut Gaze) -> Result<Value, LigError> {
-        todo!()
-    }
-}
+// pub struct HexTokenizer();
+// impl Tokenizer<Value, LigError> for HexTokenizer {
+//     fn attempt(&self, gaze: &mut Gaze) -> Result<Value, LigError> {
+//         todo!()
+//     }
+// }
 
-pub struct IntegerStep();
-impl Step<Value, LigError> for IntegerStep {
-    fn attempt(&self, gaze: &mut Gaze) -> Result<Value, LigError> {
-        todo!()
-    }
-}
+// pub struct IntegerTokenizer();
+// impl Tokenizer<Value, LigError> for IntegerTokenizer {
+//     fn attempt(&self, gaze: &mut Gaze) -> Result<Value, LigError> {
+//         todo!()
+//     }
+// }
 
-pub struct BytesStep();
-impl Step<Value, LigError> for BytesStep {
-    fn attempt(&self, gaze: &mut Gaze) -> Result<Value, LigError> {
-        todo!()
-    }
-}
+// pub struct BytesTokenizer();
+// impl Tokenizer<Value, LigError> for BytesTokenizer {
+//     fn attempt(&self, gaze: &mut Gaze) -> Result<Value, LigError> {
+//         todo!()
+//     }
+// }
 
-pub struct StringStep();
-impl Step<Value, LigError> for StringStep {
-    fn attempt(&self, gaze: &mut Gaze) -> Result<Value, LigError> {
-        todo!()
-    }
-}
+// pub struct StringTokenizer();
+// impl Tokenizer<Value, LigError> for StringTokenizer {
+//     fn attempt(&self, gaze: &mut Gaze) -> Result<Value, LigError> {
+//         todo!()
+//     }
+// }
 
-pub struct StatementStep();
-impl Step<Statement, LigError> for StatementStep {
-    fn attempt(&self, gaze: &mut Gaze) -> Result<Statement, LigError> {
-        todo!()
-    }
-}
+// pub struct StatementTokenizer();
+// impl Tokenizer<Statement, LigError> for StatementTokenizer {
+//     fn attempt(&self, gaze: &mut Gaze) -> Result<Statement, LigError> {
+//         todo!()
+//     }
+// }
