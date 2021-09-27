788fe45b98015221b18e7770f3e0ae1eddb46e50
berry3 <berry3@indiana.edu>
2021-09-27 00:41:41 -0400

    first publish of ligature-in-memory

--- ligature-in-memory/src/lib.rs
+++ ligature-in-memory/src/lib.rs
@@ -5,8 +5,16 @@
 //! This module is an implementation of Ligature that uses only
 //! in-memory persistent data structures for storing data.
 
-use rpds::RedBlackTreeSet;
+use rpds::{RedBlackTreeSet};
 
 struct LigatureInMemory {
     datasets: RedBlackTreeSet<String>
+}
+
+impl LigatureInMemory {
+    pub fn new() -> LigatureInMemory {
+        LigatureInMemory {
+            datasets: RedBlackTreeSet::new()
+        }
+    }
 }
