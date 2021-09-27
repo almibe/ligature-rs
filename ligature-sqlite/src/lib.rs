3b118975e924564b013b60627d53b87ecf6661b1
berry3 <berry3@indiana.edu>
2021-09-27 01:40:55 -0400

    starting work in sqlite support, #188

--- ligature-sqlite/src/lib.rs
+++ ligature-sqlite/src/lib.rs
@@ -1,7 +1,59 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+//! This module is the main module for the Ligature-SQLite project.
+//! It implements the traits supplied by Ligature and persists data via SQLite3.
+
+#![deny(missing_docs)]
+
+use ligature::{Ligature, WriteFn, QueryFn, LigatureError, Dataset};
+
+/// The main struct used for working with the SQLite stored version of Ligature.
+pub struct LigatureSQLite {
+    //connection:
+}
+
+impl LigatureSQLite {
+    fn create_or_open_file(path: String) -> LigatureSQLite {
+        todo!()
+    }
+
+    fn new_memory_store() -> LigatureSQLite {
+        todo!()
+    }
+}
+
+impl Ligature for LigatureSQLite {
+    fn all_datasets(&self) -> Box<dyn Iterator<Item=Result<Dataset, LigatureError>>> {
+        todo!()
+    }
+
+    fn dataset_exists(&self, dataset: &Dataset) -> Result<bool, LigatureError> {
+        todo!()
+    }
+
+    fn match_datasets_prefix(&self, prefix: &str) -> Box<dyn Iterator<Item=Result<Dataset, LigatureError>>> {
+        todo!()
+    }
+
+    fn match_datasets_range(&self, start: &str, end: &str) -> Box<dyn Iterator<Item=Result<Dataset, LigatureError>>> {
+        todo!()
+    }
+
+    fn create_dataset(&self, dataset: &Dataset) -> Result<(), LigatureError> {
+        todo!()
+    }
+
+    fn delete_dataset(&self, dataset: &Dataset) -> Result<(), LigatureError> {
+        todo!()
+    }
+
+    fn query<T>(&self, dataset: &Dataset, f: QueryFn<T>) -> Result<T, LigatureError> {
+        todo!()
+    }
+
+    fn write<T>(&self, dataset: &Dataset, f: WriteFn<T>) -> Result<T, LigatureError> {
+        todo!()
     }
 }
