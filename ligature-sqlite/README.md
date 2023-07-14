0947dff15924e3e366dbdbd0322dd9066eacdfe2
Alex Michael Berry <alexmiberry@gmail.com>
2023-07-14 05:00:15 -0400

    getting Dataset tests to pass for ligature-sqlite, #188

---
+++ ligature-sqlite/README.md
@@ -0,0 +1,13 @@
+# ligature-sqlite
+
+Datasets
+ - dataset_id: Long
+ - datasetname: String
+
+Statements
+ - statement_id: Long
+ - entity: String
+ - attribute: String
+ - value_identifier: String
+ - value_string: String
+ - value_int: Long
--- ligature-sqlite/src/lib.rs
+++ ligature-sqlite/src/lib.rs
@@ -7,53 +7,314 @@
 
 #![deny(missing_docs)]
 
-use ligature::{Ligature, WriteFn, QueryFn, LigatureError, Dataset};
+use std::{cell::RefCell, path::PathBuf, rc::Rc};
+
+use ligature::LigatureError;
+use rusqlite::{Connection, Error, params};
+use wander::{bindings::BindingsProvider, NativeFunction, WanderValue};
 
 /// The main struct used for working with the SQLite stored version of Ligature.
 pub struct LigatureSQLite {
-    //connection:
+    connection: Rc<RefCell<Connection>>,
 }
 
 impl LigatureSQLite {
-    fn create_or_open_file(path: String) -> LigatureSQLite {
+    /// Create a LigatureSQLite instance by opening the given file,
+    /// or creating a new instance if that file doesn't exist.
+    pub fn create_or_open_file(path: PathBuf) -> LigatureSQLite {
         todo!()
     }
 
-    fn new_memory_store() -> LigatureSQLite {
-        todo!()
+    /// Create a new instance of LigatureSQLite that is stored in-memory only.
+    pub fn new_memory_store() -> Result<LigatureSQLite, rusqlite::Error> {
+        let instance = LigatureSQLite {
+            connection: Rc::new(RefCell::new(Connection::open_in_memory()?)),
+        };
+        instance.setup()?;
+        Ok(instance)
+    }
+
+    fn setup(&self) -> Result<(), rusqlite::Error> {
+        self.connection.borrow().execute(
+            r#"
+            create table dataset(
+                id integer primary key, 
+                name text not null
+            );
+        "#,
+            (),
+        )?;
+        self.connection.borrow().execute(
+            r#"
+            create table statement(
+                id integer primary key, 
+                dataset_id integer not null, 
+                entity text not null,
+                attribute text not null,
+                value_int integer,
+                value_string text,
+                value_identifier text
+            );
+        "#,
+            (),
+        )?;
+        Ok(())
     }
 }
 
-impl Ligature for LigatureSQLite {
-    fn all_datasets(&self) -> Box<dyn Iterator<Item=Result<Dataset, LigatureError>>> {
-        todo!()
+impl BindingsProvider for LigatureSQLite {
+    fn add_bindings(&self, bindings: &mut wander::bindings::Bindings) {
+        bindings.bind_native_function(
+            String::from("datasets"),
+            Rc::new(DatasetsFunction {
+                connection: self.connection.clone(),
+            }),
+        );
+        bindings.bind_native_function(
+            String::from("addDataset"),
+            Rc::new(AddDatasetFunction {
+                connection: self.connection.clone(),
+            }),
+        );
+        bindings.bind_native_function(
+            String::from("removeDataset"),
+            Rc::new(RemoveDatasetFunction {
+                connection: self.connection.clone(),
+            }),
+        );
+        bindings.bind_native_function(
+            String::from("statements"),
+            Rc::new(StatementsFunction {
+                connection: self.connection.clone(),
+            }),
+        );
+        bindings.bind_native_function(
+            String::from("addStatements"),
+            Rc::new(AddStatementsFunction {
+                connection: self.connection.clone(),
+            }),
+        );
+        bindings.bind_native_function(
+            String::from("removeStatements"),
+            Rc::new(RemoveStatementsFunction {
+                connection: self.connection.clone(),
+            }),
+        );
+        bindings.bind_native_function(
+            String::from("match"),
+            Rc::new(MatchFunction {
+                connection: self.connection.clone(),
+            }),
+        );
     }
+}
 
-    fn dataset_exists(&self, dataset: &Dataset) -> Result<bool, LigatureError> {
-        todo!()
+struct DatasetsFunction {
+    connection: Rc<RefCell<Connection>>,
+}
+impl NativeFunction for DatasetsFunction {
+    fn run(
+        &self,
+        arguments: &Vec<wander::WanderValue>,
+    ) -> Result<wander::WanderValue, LigatureError> {
+        if arguments.is_empty() {
+            let connection = self.connection.borrow();
+            let mut stmt = connection.prepare("select name from dataset").unwrap();
+            let iter = stmt
+                .query_map([], |row| {
+                    let x: String = row.get(0)?;
+                    Ok(x)
+                })
+                .unwrap();
+            let mut results: Vec<WanderValue> = vec![];
+            for name in iter {
+                results.push(WanderValue::String(name.unwrap()));
+            }
+            Ok(WanderValue::List(results))
+        } else {
+            Err(LigatureError(
+                "`datasets` function take no arguments.".to_owned(),
+            ))
+        }
     }
+}
 
-    fn match_datasets_prefix(&self, prefix: &str) -> Box<dyn Iterator<Item=Result<Dataset, LigatureError>>> {
-        todo!()
+struct AddDatasetFunction {
+    connection: Rc<RefCell<Connection>>,
+}
+impl NativeFunction for AddDatasetFunction {
+    fn run(
+        &self,
+        arguments: &Vec<wander::WanderValue>,
+    ) -> Result<wander::WanderValue, LigatureError> {
+        match &arguments[..] {
+            [WanderValue::String(name)] => {
+                self.connection
+                    .borrow()
+                    .execute("insert into dataset (name) values (?1)", [name])
+                    .unwrap();
+                Ok(WanderValue::Nothing)
+            }
+            _ => todo!(),
+        }
     }
+}
 
-    fn match_datasets_range(&self, start: &str, end: &str) -> Box<dyn Iterator<Item=Result<Dataset, LigatureError>>> {
-        todo!()
+struct RemoveDatasetFunction {
+    connection: Rc<RefCell<Connection>>,
+}
+impl NativeFunction for RemoveDatasetFunction {
+    fn run(
+        &self,
+        arguments: &Vec<wander::WanderValue>,
+    ) -> Result<wander::WanderValue, LigatureError> {
+        match &arguments[..] {
+            [WanderValue::String(name)] => {
+                self.connection
+                    .borrow()
+                    .execute("delete from dataset where name =?1", [name])
+                    .unwrap();
+                Ok(WanderValue::Nothing)
+            }
+            _ => todo!(),
+        }
     }
+}
 
-    fn create_dataset(&self, dataset: &Dataset) -> Result<(), LigatureError> {
-        todo!()
+struct StatementsFunction {
+    connection: Rc<RefCell<Connection>>,
+}
+impl NativeFunction for StatementsFunction {
+    fn run(
+        &self,
+        arguments: &Vec<wander::WanderValue>,
+    ) -> Result<wander::WanderValue, LigatureError> {
+        match &arguments[..] {
+            [WanderValue::String(name)] => {
+                let connection = self.connection.borrow();
+                let mut stmt = connection.prepare(
+                    "select entity, attribute, value_identifier from statement inner join dataset on statement.dataset_id = dataset.id").unwrap();
+                let iter = stmt
+                    .query_map([], |row| {
+                        let entity: String = row.get(0)?;
+                        let attribute = row.get(1)?;
+                        let value = row.get(2)?;
+                        Ok(WanderValue::List(vec![
+                            WanderValue::String(entity),
+                            WanderValue::String(attribute),
+                            WanderValue::String(value),
+                        ]))
+                    })
+                    .unwrap();
+                let mut results: Vec<WanderValue> = vec![];
+                for statement in iter {
+                    results.push(statement.unwrap());
+                }
+                Ok(WanderValue::List(results))
+            }
+            _ => todo!(),
+        }
     }
+}
 
-    fn delete_dataset(&self, dataset: &Dataset) -> Result<(), LigatureError> {
-        todo!()
+struct AddStatementsFunction {
+    connection: Rc<RefCell<Connection>>,
+}
+impl NativeFunction for AddStatementsFunction {
+    fn run(
+        &self,
+        arguments: &Vec<wander::WanderValue>,
+    ) -> Result<wander::WanderValue, LigatureError> {
+        match &arguments[..] {
+            [WanderValue::String(name), WanderValue::List(statements)] => {
+                let mut connection = self.connection.borrow_mut();
+                let mut tx = connection.transaction().unwrap();
+                let id = tx
+                    .query_row_and_then("select id from dataset where name = ?1", [name], |row| {
+                        let id: i64 = row.get(0).unwrap();
+                        Ok::<i64, Error>(id)
+                    })
+                    .unwrap();
+                statements.iter().for_each(|statement| {
+                    if let WanderValue::List(contents) = statement {
+                        match &contents[..] {
+                            [WanderValue::Identifier(entity), WanderValue::Identifier(attribute), x] => {
+                                let entity = entity.id();
+                                let attribute = attribute.id();
+                                let value = if let WanderValue::Identifier(value) = x {
+                                    value.id()
+                                } else {
+                                    todo!();
+                                };
+                                tx.execute("insert into statement (dataset_id, entity, attribute, value_identifier) values (?1, ?2, ?3, ?4)", params![id, entity, attribute, value]).unwrap();
+                            },
+                            _ => todo!()
+                        }
+                    } else {
+                        todo!()
+                    }
+                });
+                tx.commit().unwrap();
+                Ok(WanderValue::Nothing)
+            }
+            _ => todo!(),
+        }
     }
+}
 
-    fn query<T>(&self, dataset: &Dataset, f: QueryFn<T>) -> Result<T, LigatureError> {
-        todo!()
+struct RemoveStatementsFunction {
+    connection: Rc<RefCell<Connection>>,
+}
+impl NativeFunction for RemoveStatementsFunction {
+    fn run(
+        &self,
+        arguments: &Vec<wander::WanderValue>,
+    ) -> Result<wander::WanderValue, LigatureError> {
+        match &arguments[..] {
+            [WanderValue::String(name), WanderValue::List(statements)] => {
+                let mut connection = self.connection.borrow_mut();
+                let mut tx = connection.transaction().unwrap();
+                let id = tx
+                    .query_row_and_then("select id from dataset where name = ?1", [name], |row| {
+                        let id: i64 = row.get(0).unwrap();
+                        Ok::<i64, Error>(id)
+                    })
+                    .unwrap();
+                statements.iter().for_each(|statement| {
+                    if let WanderValue::List(contents) = statement {
+                        match &contents[..] {
+                            [WanderValue::Identifier(entity), WanderValue::Identifier(attribute), x] => {
+                                let entity = entity.id();
+                                let attribute = attribute.id();
+                                let value = if let WanderValue::Identifier(value) = x {
+                                    value.id()
+                                } else {
+                                    todo!();
+                                };
+                                tx.execute("delete from statement where dataset_id = ?1 and entity = ?2 and attribute = ?3 and value_identifier = ?4", params![id, entity, attribute, value]).unwrap();
+                            },
+                            _ => todo!()
+                        }
+                    } else {
+                        todo!()
+                    }
+                });
+                tx.commit().unwrap();
+                Ok(WanderValue::Nothing)
+            }
+            _ => todo!(),
+        }
     }
+}
 
-    fn write<T>(&self, dataset: &Dataset, f: WriteFn<T>) -> Result<T, LigatureError> {
+struct MatchFunction {
+    connection: Rc<RefCell<Connection>>,
+}
+impl NativeFunction for MatchFunction {
+    fn run(
+        &self,
+        arguments: &Vec<wander::WanderValue>,
+    ) -> Result<wander::WanderValue, LigatureError> {
         todo!()
     }
 }
