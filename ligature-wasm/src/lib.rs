0b88e92361f2d19901c0b3cceaa4830bf12cfbc5
Alex Michael Berry <alexmiberry@gmail.com>
2023-07-25 01:03:57 -0400

    first pass at adding api to ligature-wasm, #257

--- ligature-wasm/src/lib.rs
+++ ligature-wasm/src/lib.rs
@@ -1,13 +1,21 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+//! This project exposes functionality from the Rust implementation of Ligature to WASM and JS runtimes thanks to wasm-bindgen and wasm-pack.
+
 mod utils;
 
+use ligature_in_memory::LigatureInMemory;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, ligature-wasm!");
+pub fn run(script: String) -> String {
+    let mut bindings = wander::preludes::common();
+    let instance = LigatureInMemory::new();
+    instance.add_bindings(&mut bindings);
+    match wander::run(&script, &mut bindings) {
+        Ok(value) => format!("{value}"),
+        Err(error) => format!("{}", error.0)
+    }
 }
