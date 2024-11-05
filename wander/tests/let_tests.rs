b35242d5ff34214ee5b60f5e11ff3c10daa94454
Alex Michael Berry <alexmiberry@gmail.com>
2024-11-05 00:20:00 -0500

    fixing lexter tests

--- wander/tests/let_tests.rs
+++ wander/tests/let_tests.rs
@@ -6,72 +6,8 @@ use wander::{preludes::common, run, WanderValue};
 
 // #[test]
 // fn basic_let() {
-//     let input = "let val x = 5 in x end";
+//     let input = "let test {a b c}";
 //     let res = run(input, &mut common::<NoHostType>()).first().unwrap().unwrap();
-//     let expected = WanderValue::Int(5);
-//     assert_eq!(res, expected);
-// }
-
-// #[test]
-// fn basic_let_multiple_vals() {
-//     let input = "let val x = true val y = Bool.and x in y x end";
-//     let res = run(input, &mut common::<NoHostType>()).first().unwrap().unwrap();
-//     let expected = WanderValue::Bool(true);
-//     assert_eq!(res, expected);
-// }
-
-// #[test]
-// fn nested_lets() {
-//     let input = r#"
-//         let
-//           val x = true
-//           val y =
-//               let
-//                 val y1 = x
-//               in
-//                 y1
-//               end
-//         in
-//           y
-//         end"#;
-//     let res = run(input, &mut common::<NoHostType>()).first().unwrap().unwrap();
-//     let expected = WanderValue::Bool(true);
-//     assert_eq!(res, expected);
-// }
-
-// #[test]
-// fn lets_with_function_calls_in_decl() {
-//     let input = r#"
-//         let
-//             val x = true
-//             val y = Bool.and x false
-//         in
-//             y
-//         end
-//     "#;
-//     let res = run(input, &mut common::<NoHostType>()).first().unwrap().unwrap();
-//     let expected = WanderValue::Bool(false);
-//     assert_eq!(res, expected);
-// }
-
-// #[test]
-// fn nested_lets_with_function_calls() {
-//     let input = r#"
-//         let
-//           val x = true
-//           val y =
-//               let
-//                 val x = true
-//                 val y = Bool.and x false
-//               in
-//                 y
-//               end
-//         in
-//           y
-//         end
-//         "#;
-//     let res: Vec<WanderValue<NoHostType>> = run(input, &mut common::<NoHostType>())
-//       .iter().map(|t| t.unwrap()).collect();
-//     let expected = vec![WanderValue::Bool(false)];
+//     let expected = WanderValue::Network...;
 //     assert_eq!(res, expected);
 // }
--- wander/tests/lexer_tests.rs
+++ wander/tests/lexer_tests.rs
@@ -25,75 +25,60 @@ fn tokenize_booleans() {
     assert_eq!(res, expected);
 }
 
-// #[test]
-// fn tokenize_integer() {
-//     let input = "123450";
-//     let res = tokenize_and_filter(input).iter().map(|t| t.first().unwrap().0).collect();
-//     let expected = Ok(vec![Token::Int(123450)]);
-//     assert_eq!(res, expected);
-// }
+#[test]
+fn tokenize_integer() {
+    let input = "123450";
+    let res: Vec<Token> = tokenize_and_filter(input).iter().map(|t| t.first().unwrap().0.clone()).collect();
+    let expected = vec![Token::Element(Element("123450".to_owned()))];
+    assert_eq!(res, expected);
+}
 
-// #[test]
-// fn tokenize_integers() {
-//     let input = "0 -100 4200";
-//     let res: Vec<_> = tokenize_and_filter(input).unwrap().iter().map(|t| t.0).collect();
-//     let expected = vec![Token::Int(0), Token::Int(-100), Token::Int(4200)];
-//     assert_eq!(res, expected);
-// }
+#[test]
+fn tokenize_integers() {
+    let input = "0 -100 4200";
+    let res: Vec<_> = tokenize_and_filter(input).unwrap().iter().map(|t| t.0.clone()).collect();
+    let expected = vec![Token::Element(Element("0".to_owned())), Token::Element(Element("-100".to_owned())), Token::Element(Element("4200".to_owned()))];
+    assert_eq!(res, expected);
+}
 
-// #[test]
-// fn tokenize_strings() {
-//     let input = "\"Hello, world\"";
-//     let res = tokenize_and_filter(input).unwrap().first().unwrap().0;
-//     let expected = Token::String(String::from("Hello, world"));
-//     assert_eq!(res, expected);
-// }
+#[test]
+fn tokenize_strings() {
+    let input = "\"Hello, world\"";
+    let res = tokenize_and_filter(input).unwrap().first().unwrap().0.clone();
+    let expected = Token::String("Hello, world".to_owned());
+    assert_eq!(res, expected);
+}
 
-// #[test]
-// fn tokenize_strings_with_quotes() {
-//     let input = "\"\\\"Hello, world\\\"\"";
-//     let res = tokenize_and_filter(input).unwrap().first().unwrap().0;
-//     let expected = Token::String(String::from("\\\"Hello, world\\\""));
-//     assert_eq!(res, expected);
-// }
+#[test]
+fn tokenize_strings_with_quotes() {
+    let input = "\"\\\"Hello, world\\\"\"";
+    let res = tokenize_and_filter(input).unwrap().first().unwrap().0.clone();
+    let expected = Token::String(String::from("\\\"Hello, world\\\""));
+    assert_eq!(res, expected);
+}
 
-// #[test]
-// fn tokenize_name() {
-//     let input = "hello123";
-//     let res = tokenize_and_filter(input).unwrap().first().unwrap().0;
-//     let expected = Token::Name(String::from("hello123"));
-//     assert_eq!(res, expected);
-// }
+#[test]
+fn tokenize_name() {
+    let input = "hello123";
+    let res = tokenize_and_filter(input).unwrap().first().unwrap().0.clone();
+    let expected = Token::Element(Element("hello123".to_owned()));
+    assert_eq!(res, expected);
+}
 
-// #[test]
-// fn tokenize_names_keywords_and_symbols() {
-//     let input = "val x = 5";
-//     let res: Vec<Token> = tokenize_and_filter(input).unwrap().iter().map(|t| t.0).collect();
-//     let expected = vec![
-//         Token::Val,
-//         Token::Name(String::from("x")),
-//         Token::EqualSign,
-//         Token::Int(5),
-//     ];
-//     assert_eq!(res, expected);
-// }
-
-// #[test]
-// fn tokenize_function_call() {
-//     let input = "not(false)";
-//     let res: Vec<Token> = tokenize_and_filter(input).unwrap().iter().map(|t| t.0).collect();
-//     let expected = vec![
-//         Token::Name(String::from("not")),
-//         Token::OpenParen,
-//         Token::Boolean(false),
-//         Token::CloseParen,
-//     ];
-//     assert_eq!(res, expected);
-// }
+#[test]
+fn tokenize_function_call() {
+    let input = "not false";
+    let res: Vec<Token> = tokenize_and_filter(input).unwrap().iter().map(|t| t.0.clone()).collect();
+    let expected = vec![
+        Token::Element(Element("not".to_owned())),
+        Token::Element(Element("false".to_owned())),
+    ];
+    assert_eq!(res, expected);
+}
 
 // #[test]
 // fn tokenize_function_call_with_pipe() {
-//     let input = "false | not()";
+//     let input = "false | not";
 //     let res = tokenize_and_filter(input);
 //     let expected = Ok(vec![
 //         Location(Token::Boolean(false), 0),
@@ -105,26 +90,26 @@ fn tokenize_booleans() {
 //     assert_eq!(res, expected);
 // }
 
-// #[test]
-// fn tokenize_comment() {
-//     let input = "--hello";
-//     let res = tokenize_and_filter(input);
-//     let expected = Ok(vec![]);
-//     assert_eq!(res, expected);
-// }
+#[test]
+fn tokenize_comment() {
+    let input = "--hello";
+    let res = tokenize_and_filter(input);
+    let expected = Ok(vec![]);
+    assert_eq!(res, expected);
+}
 
-// #[test]
-// fn tokenize_complex_comment() {
-//     let input = "-- <<<>>> () {} }{ )( ><";
-//     let res = tokenize_and_filter(input);
-//     let expected = Ok(vec![]);
-//     assert_eq!(res, expected);
-// }
+#[test]
+fn tokenize_complex_comment() {
+    let input = "-- <<<>>> () {} }{ )( ><";
+    let res = tokenize_and_filter(input);
+    let expected = Ok(vec![]);
+    assert_eq!(res, expected);
+}
 
-// #[test]
-// fn multiline_comment() {
-//     let input = "-- <<<>>> () {} }{ )( ><\n5--five\n--comment";
-//     let res = tokenize_and_filter(input);
-//     let expected = Ok(vec![Location(Token::Int(5), 0)]);
-//     assert_eq!(res, expected);
-// }
+#[test]
+fn multiline_comment() {
+    let input = "-- <<<>>> () {} }{ )( ><\n5 -- five\n-- comment";
+    let res: Vec<Token> = tokenize_and_filter(input).unwrap().iter().map(|t| t.0.clone()).collect();;
+    let expected = vec![Token::Element(Element("5".to_owned()))];
+    assert_eq!(res, expected);
+}
