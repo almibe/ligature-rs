34d6d2030870e7ad3b9271a76d0b81dc7deb645c
Alex Michael Berry <alexmiberry@gmail.com>
2024-11-14 05:35:22 -0500

    adding api to ligature

--- ligature/src/lib.rs
+++ ligature/src/lib.rs
@@ -6,6 +6,9 @@
 
 #![deny(missing_docs)]
 
+use std::collections::{BTreeMap, BTreeSet};
+use hashbag::HashBag;
+
 use serde::{Deserialize, Serialize};
 
 /// An Element that is identified by a unique String value.
@@ -38,4 +41,37 @@ pub enum Entry {
         /// The Concept the Element does not extend.
         concept: Element,
     },
+}
+
+/// A trait that defines all the actions a Ligature instance can perform.
+/// The API used for storing triples.
+pub trait Ligature<E> {
+    /// Get all Collections.
+    fn collections(&self) -> Result<Vec<Element>, E>;
+
+    /// Add a new Dataset.
+    /// Does nothing if Dataset already exists.
+    fn add_collection(&mut self, collection: Element) -> Result<(), E>;
+
+    /// Remove a Dataset.
+    /// Does nothing if Dataset doesn't exist.
+    fn remove_collection(&mut self, collection: Element) -> Result<(), E>;
+
+    /// Get all Statements in a given Dataset.
+    fn entries(&self, collection: Element) -> Result<BTreeSet<Entry>, E>;
+
+    /// Add Statements to a given Dataset.
+    /// Returns Error if Dataset doesn't exist.
+    /// Does nothing if Statement already exists in Dataset.
+    fn add_entries(&mut self, collection: Element, entries: &mut BTreeSet<Entry>) -> Result<(), E>;
+    /// Remove Statements from a given Dataset.
+    /// Returns Error if Dataset doesn't exist.
+    /// Does nothing if Statement doesn't exist in Dataset.
+    fn remove_entries(&mut self, collection: Element, entries: &mut BTreeSet<Entry>) -> Result<(), E>;
+    /// Run a query against the given Dataset.
+    fn query(
+        &self,
+        collection: Element,
+        pattern: BTreeSet<Entry>,
+    ) -> Result<HashBag<BTreeMap<String, Element>>, E>;
 }
