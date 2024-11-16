99a64aa620669ee572605b1d8c59d8d8bb333c21
Alex Michael Berry <alexmiberry@gmail.com>
2024-11-15 22:56:10 -0500

    removing old code from ligature-repl

--- ligature-repl/src/main.rs
+++ ligature-repl/src/main.rs
@@ -3,20 +3,3 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 //! This module is the main module for the ligature-repl project.
-
-use ligature_in_memory::LigatureInMemory;
-use wander_repl::{start_repl, REPLState};
-use rustyline::Result;
-use wander::preludes::common;
-use wander::NoHostType;
-use ligature_wander::bind_instance;
-use std::rc::Rc;
-use std::sync::RwLock;
-
-fn main() -> Result<()> {
-    let mut bindings = common::<NoHostType>();
-    let instance = Rc::new(RwLock::new(LigatureInMemory::new()));
-    bind_instance(instance, &mut bindings);
-    let mut state = REPLState { bindings };
-    start_repl(&mut state)
-}
