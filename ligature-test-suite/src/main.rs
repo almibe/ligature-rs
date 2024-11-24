17545e46861c4824fd98667505edace11cb1bb04
Alex Michael Berry <alexmiberry@gmail.com>
2024-11-24 04:48:40 -0500

    fixing ligature-test-suite

--- ligature-test-suite/src/main.rs
+++ ligature-test-suite/src/main.rs
@@ -3,3 +3,20 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 //! This module is an implementation of the a test suite for Ligature implementations.
+
+fn main() {
+    println!("Running Ligature Test Suite!");
+    let location = std::env::var("LIGATURE_TEST_SUITE").unwrap();
+    println!("Running tests in {}.", location);
+    for path in std::fs::read_dir(location).unwrap() {
+        let path = path.unwrap();
+        if path.file_name().into_string().unwrap().ends_with(".wander") {
+            println!(" - Starting {:?}...", path);
+            let script = std::fs::read_to_string(path.path()).unwrap();
+            match wander::run(&script, HashMap::new(), &mut ligature_graph::LigatureGraph::new()) {
+                Ok(_) => println!(" - Success!"),
+                Err(err) => println!(" - FAILED! {:?}", err)
+            }
+        }
+    }
+}
