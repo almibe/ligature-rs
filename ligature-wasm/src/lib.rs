9d1e6828878613af994fc1e2647a2cec66c7d077
Alex Michael Berry <alexmiberry@gmail.com>
2024-11-28 03:36:08 -0500

    updating ligature-wasm to use graphology api

--- ligature-wasm/src/lib.rs
+++ ligature-wasm/src/lib.rs
@@ -5,24 +5,112 @@
 //! This project exposes functionality from the Rust implementation of Wander to WASM and JS runtimes thanks to wasm-bindgen and wasm-pack.
 
 mod utils;
+use std::collections::{self, BTreeSet, HashMap};
+
+use ligature::{Entry, Ligature};
+use ligature_graph::LigatureGraph;
+use serde::Serialize;
 use wander::{WanderError, WanderValue};
 use wasm_bindgen::prelude::*;
-use serde::Serialize;
-use ligature_graph::LigatureGraph;
 
-#[wasm_bindgen]
-pub fn run(script: String) -> JsValue {
-    let mut bindings = wander::prelude::common();
-    let res = wander::run(&script,  &bindings, &mut LigatureGraph::new());
-    let res = RunResult {
-        object: res.clone(),
-        string: res.map(|res| format!("{}", res)),
-    };
+#[derive(Serialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct GraphologyResult {
+    nodes: BTreeSet<NodeResult>,
+    edges: BTreeSet<EdgeResult>,
+}
+
+#[derive(Serialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct NodeResult {
+    key: String,
+}
+
+#[derive(Serialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct EdgeResult {
+    key: String,
+    source: String,
+    target: String,
+}
+
+fn graph_to_graphology(graph: BTreeSet<Entry>) -> GraphologyResult {
+    let mut nodes = BTreeSet::new();
+    let mut edges = BTreeSet::new();
+    for entry in graph {
+        match entry {
+            Entry::Extends { element, concept } => {
+                nodes.insert(NodeResult {
+                    key: element.clone().0,
+                });
+                nodes.insert(NodeResult {
+                    key: concept.clone().0,
+                });
+                edges.insert(EdgeResult {
+                    key: ":".to_owned(),
+                    source: element.0,
+                    target: concept.0,
+                });
+            }
+            Entry::NotExtends { element, concept } => {
+                nodes.insert(NodeResult {
+                    key: element.clone().0,
+                });
+                nodes.insert(NodeResult {
+                    key: concept.clone().0,
+                });
+                edges.insert(EdgeResult {
+                    key: "¬:".to_owned(),
+                    source: element.0,
+                    target: concept.0,
+                });
+            }
+            Entry::Role {
+                first,
+                second,
+                role,
+            } => {
+                nodes.insert(NodeResult {
+                    key: first.clone().0,
+                });
+                nodes.insert(NodeResult {
+                    key: second.clone().0,
+                });
+                edges.insert(EdgeResult {
+                    key: role.0,
+                    source: first.0,
+                    target: second.0,
+                });
+            }
+        }
+    }
+    GraphologyResult {
+        nodes: nodes,
+        edges: edges,
+    }
+}
+
+pub fn ligature_to_graphology(ligature: &dyn Ligature) -> JsValue {
+    let mut res = HashMap::new();
+    match ligature.collections() {
+        Ok(collections) => {
+            for collection in collections {
+                match ligature.entries(&collection) {
+                    Ok(entries) => {
+                        res.insert(collection.0, graph_to_graphology(entries));
+                    }
+                    Err(_) => todo!(),
+                }
+            }
+        }
+        Err(err) => todo!(),
+    }
     serde_wasm_bindgen::to_value(&res).unwrap()
 }
 
-#[derive(Serialize)]
-pub struct RunResult {
-    object: Result<WanderValue, WanderError>,
-    string: Result<String, WanderError>,
+#[wasm_bindgen]
+pub fn run(script: String) -> JsValue {
+    let mut state = LigatureGraph::new();
+    let mut bindings = wander::prelude::common();
+    match wander::run(&script, &bindings, &mut state) {
+        Ok(_) => ligature_to_graphology(&state),
+        Err(err) => serde_wasm_bindgen::to_value(&err).unwrap(),
+    }
 }
